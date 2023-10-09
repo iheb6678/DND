d73f161bbbe4d7632a0c327ea9f6533842b03023
iheb667 <ihebnoumi2@gmail.com>
2023-10-09 10:16:31 +0100

    dnd

--- dnd/src/App.js
+++ dnd/src/App.js
@@ -1,68 +1,189 @@
-// src/App.js
 import React, { useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import './App.css';
-import { useDrag, useDrop } from 'react-dnd';
 
-const ItemType = 'BOX';
+const ItemTypes = {
+  COMPONENT: 'component',
+};
 
-const Box = ({ name }) => {
-  const [{ isDragging }, drag] = useDrag({
-    type: ItemType,
-    item: { name },
-    collect: (monitor) => ({
-      isDragging: monitor.isDragging(),
-    }),
-  });
-
+const Component = ({ id, name, index, components, onDragEnd, onAddComponent }) => {
   return (
-    <div
-      ref={drag}
-      className={`box ${isDragging ? 'dragging' : ''}`}
+    <Draggable
+      draggableId={id}
+      index={index}
+      onStart={() => onAddComponent(id)}
     >
-      {name}
-    </div>
+      {(provided) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          className="component"
+        >
+          {name}
+          <Droppable droppableId={id} type={ItemTypes.COMPONENT}>
+            {(provided) => (
+              <div ref={provided.innerRef} {...provided.droppableProps}>
+                {components.map((child, childIndex) => (
+                  <Component
+                    key={child.id}
+                    id={child.id}
+                    name={child.name}
+                    index={childIndex}
+                    components={child.components}
+                    onDragEnd={onDragEnd}
+                    onAddComponent={onAddComponent}
+                  />
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </div>
+      )}
+    </Draggable>
   );
 };
 
-const Target = ({ onDrop, children }) => {
-  const [, drop] = useDrop({
-    accept: ItemType,
-    drop: (item) => onDrop(item),
-  });
-
-  return (
-    <div ref={drop} className="target">
-      {children}
-    </div>
-  );
+const ZonesData = {
+  'Zone A': {
+    allowedTypes: ['Type A'],
+    components: [
+      {
+        id: '1',
+        name: 'Component 1',
+        type: 'Type A',
+        components: [],
+      },
+      {
+        id: '4',
+        name: 'Component 4',
+        type: 'Type A',
+        components: [],
+      },
+    ],
+  },
+  'Zone B': {
+    allowedTypes: ['Type B'],
+    components: [
+      {
+        id: '3',
+        name: 'Component 3',
+        type: 'Type B',
+        components: [],
+      },
+      {
+        id: '2',
+        name: 'Component 2',
+        type: 'Type B',
+        components: [],
+      },
+    ],
+  },
+  'Zone C': {
+    allowedTypes: ['Type A', 'Type B'],
+    components: [],
+  },
 };
 
-function App() {
-  const [boxes, setBoxes] = useState([]);
+const App = () => {
+  const [zones, setZones] = useState(ZonesData);
+  const [draggedComponent, setDraggedComponent] = useState(null);
 
-  const handleDrop = (item) => {
-    setBoxes([...boxes, item]);
+  const onDragEnd = (result) => {
+    if (!result.destination) return;
+
+    const sourceZoneId = result.source.droppableId;
+    const destinationZoneId = result.destination.droppableId;
+    const sourceZone = zones[sourceZoneId];
+    const destinationZone = zones[destinationZoneId];
+    const movedComponent = sourceZone.components[result.source.index];
+
+    // Check if the component type is allowed in the destination zone
+    if (
+      !destinationZone ||
+      !destinationZone.allowedTypes.includes(movedComponent.type)
+    )
+      return;
+
+    const updatedZones = { ...zones };
+    const sourceComponents = updatedZones[sourceZoneId].components;
+    const destinationComponents = updatedZones[destinationZoneId].components;
+
+    // Remove the component from the source zone
+    sourceComponents.splice(result.source.index, 1);
+
+    // Insert the component into the destination zone
+    destinationComponents.splice(result.destination.index, 0, movedComponent);
+
+    setZones(updatedZones);
+  };
+
+  const onAddComponent = (parentId) => {
+    const newComponentId = Date.now().toString();
+    const newComponent = {
+      id: newComponentId,
+      name: `New Component ${newComponentId}`,
+      type: 'Type A', // You can change the type as needed
+      components: [],
+    };
+
+    const updatedZones = { ...zones };
+    const parentZone = Object.values(updatedZones).find((zone) =>
+      zone.components.find((component) => component.id === parentId)
+    );
+
+    if (parentZone) {
+      const parentComponent = parentZone.components.find(
+        (component) => component.id === parentId
+      );
+      parentComponent.components.push(newComponent);
+      setZones(updatedZones);
+    }
   };
 
   return (
-    <div className="App">
-      <h1>Drag and Drop Example</h1>
-      <div className="container">
-        <div className="source-container">
-          <Box name="Item 1" />
-          <Box name="Item 2" />
-          <Box name="Item 3" />
+    <div className="container">
+      <DragDropContext onDragEnd={onDragEnd}>
+        {Object.entries(zones).map(([zoneName, zone]) => (
+          <Droppable
+            key={zoneName}
+            droppableId={zoneName}
+            type={ItemTypes.COMPONENT}
+          >
+            {(provided) => (
+              <div
+                className="zone"
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+              >
+                <h3>{zoneName} Zone</h3>
+                {zone.components.map((component, index) => (
+                  <Component
+                    key={component.id}
+                    id={component.id}
+                    name={component.name}
+                    index={index}
+                    components={component.components}
+                    onDragEnd={onDragEnd}
+                    onAddComponent={onAddComponent}
+                  />
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        ))}
+      </DragDropContext>
+
+      {/* Render the dragged component as a placeholder */}
+      {draggedComponent && (
+        <div className="dragged-component">
+          {draggedComponent.name}
         </div>
-        <div className="target-container">
-          <Target onDrop={handleDrop}>
-            {boxes.map((box, index) => (
-              <Box key={index} name={box.name} />
-            ))}
-          </Target>
-        </div>
-      </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
